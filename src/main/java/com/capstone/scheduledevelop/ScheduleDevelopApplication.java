a4139f45f8d0c252998614745bd291a079ca07db
kimwonjun1 <monkeydmw@naver.com>
2025-05-20 14:46:12 +0900

    project create

---
+++ src/main/java/com/capstone/scheduledevelop/ScheduleDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.scheduledevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDevelopApplication.class, args);
+    }
+
+}
