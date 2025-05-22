199ad0d22f63d0dac497073ef404472e1c8af9e2
kimwonjun1 <monkeydmw@naver.com>
2025-05-22 14:50:43 +0900

    SignUp Dto create

--- src/main/java/com/capstone/scheduledevelop/dto/SignUpRequestDto.java
+++ src/main/java/com/capstone/scheduledevelop/dto/SignUpRequestDto.java
@@ -1,2 +1,19 @@
-package com.capstone.scheduledevelop.dto;public class SignUpRequestDto {
+package com.capstone.scheduledevelop.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SignUpRequestDto {
+
+    private final String name;
+
+    private final String email;
+
+    private final String password;
+
+    public SignUpRequestDto(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/main/java/com/capstone/scheduledevelop/dto/SignUpResponseDto.java
+++ src/main/java/com/capstone/scheduledevelop/dto/SignUpResponseDto.java
@@ -1,2 +1,19 @@
-package com.capstone.scheduledevelop.dto;public class SignUpResponseDto {
+package com.capstone.scheduledevelop.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SignUpResponseDto {
+
+    private final Long id;
+
+    private final String name;
+
+    private final String email;
+
+    public SignUpResponseDto(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
