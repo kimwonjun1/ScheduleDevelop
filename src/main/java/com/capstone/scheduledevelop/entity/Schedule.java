9ce2946b86c1d4a2432c4c347970dc659a0d2bf6
kimwonjun1 <monkeydmw@naver.com>
2025-05-22 14:36:24 +0900

    User, Schedule entity create

--- src/main/java/com/capstone/scheduledevelop/entity/Schedule.java
+++ src/main/java/com/capstone/scheduledevelop/entity/Schedule.java
@@ -1,2 +1,24 @@
-package com.capstone.scheduledevelop.entity;public class Schedule {
+package com.capstone.scheduledevelop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
 }
--- src/main/java/com/capstone/scheduledevelop/entity/User.java
+++ src/main/java/com/capstone/scheduledevelop/entity/User.java
@@ -1,2 +1,22 @@
-package com.capstone.scheduledevelop.entity;public class User {
+package com.capstone.scheduledevelop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
 }
