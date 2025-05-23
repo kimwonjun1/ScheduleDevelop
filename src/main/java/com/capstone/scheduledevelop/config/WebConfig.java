3f40f7c24b92902cea9fac6b238fabbee40be3ca
kimwonjun1 <monkeydmw@naver.com>
2025-05-23 15:54:22 +0900

    filter add

---
+++ src/main/java/com/capstone/scheduledevelop/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.capstone.scheduledevelop.config;
+
+import com.capstone.scheduledevelop.filter.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterFilterRegistrationBean.setFilter(new LoginFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+
+    }
+}
